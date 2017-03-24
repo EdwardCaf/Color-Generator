379a13466f46ae0e5ab8d29e2198d448c82ed407
EdCaf <Ed_caf@protonmail.com>
2017-03-24 00:33:43 -0400

    Add Randomize button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-$('.col').each(function (min,max) {
-  min = Math.ceil(1);
-  max = Math.floor(3);
-  num = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log(num);
-  if (num === 1) {
-    $(this).css({backgroundColor: "#4f3" });
-  }
-  if (num === 2) {
-    $(this).css({backgroundColor: "#912" });
-  }
-  if (num === 3) {
-    $(this).css({backgroundColor: "#22f" });
-  }
-  return num;
-});
+function Randomize() {
+  $('.col').each(function (min,max) {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    num = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(num);
+    if (num === 1) {
+      $(this).css({backgroundColor: "#3c3" });
+    }
+    if (num === 2) {
+      $(this).css({backgroundColor: "#d12" });
+    }
+    if (num === 3) {
+      $(this).css({backgroundColor: "#22f" });
+    }
+    if (num === 4) {
+      $(this).css({backgroundColor: "#ff4" });
+    }
+    if (num === 5) {
+      $(this).css({backgroundColor: "#fff" });
+    }
+    if (num === 6) {
+      $(this).css({backgroundColor: "#f90" });
+    }
+    return num;
+  });
+};
